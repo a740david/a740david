eb631957eba87781595439e288bb538f5551c77f
a740david <54502632+a740david@users.noreply.github.com>
2020-05-14 14:21:30 +0300

    add1

---
+++ Desktop/e/src/components/FilePicker.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Uppy from '@uppy/core';
+import GoogleDrive from '@uppy/google-drive';
+import { Dashboard } from '@uppy/react'
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+
+	
+	
+const uppy = Uppy({
+	restrictions: {
+		allowedFileTypes: ['.csv']
+	}
+})
+	.use(GoogleDrive, { id: 'GoogleDrive', companionUrl: 'https://companion.uppy.io' })
+
+const FilePicker = ({ onChange }) => {
+	uppy.on('complete', (result) => {
+		onChange(result.successful[0].data);
+	});
+
+	return (
+		<Dashboard 
+			uppy={uppy}
+			plugins={['GoogleDrive']}>
+		</Dashboard>
+	)
+}
+
+export default FilePicker;
